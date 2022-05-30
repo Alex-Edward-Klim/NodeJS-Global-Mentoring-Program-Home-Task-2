4d9d9eff9411ac45c6e28bd2fa12cb72509d9a68
Alexey Klimovskoy <Alexey_Klimovskoy@epam.com>
2022-05-30 16:54:57 +0300

    fix: remove body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 import usersRoutes from './routes/users';
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/users', usersRoutes);
 
